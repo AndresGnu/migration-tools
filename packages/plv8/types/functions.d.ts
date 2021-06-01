c02658d50cf00c1ba5674d7ccb34a374d62afebb
Andres Navarrete <anavarrete@monkeyplusbc.com>
2021-06-01 00:46:23 -0500

    up

--- packages/plv8/types/functions.d.ts
+++ packages/plv8/types/functions.d.ts
@@ -1,12 +1,13 @@
+import { PLV8 } from './index';
 export type ContextFunction<Args = any> = {
   ARGS: Args;
-  plv8: any;
+  plv8: PLV8;
 };
 
-export type ContextTriggeFun<ARGS = any, TABLE = any, TG_ARGS = string[]> = {
-  plv8: any;
+export type ContextTriggeFun<ARGS = any, ROW = any, TG_ARGS = string[]> = {
+  plv8: PLV8;
   ARGS: ARGS;
-  NEW: TABLE;
-  OLD: TABLE;
+  NEW: ROW;
+  OLD: ROW;
   TG_OP: 'INSERT' | 'UPDATE' | 'DELETE' | 'TRUNCATE';
 };
